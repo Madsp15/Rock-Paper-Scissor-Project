b7da238369e8c0254bd7dc066d0625dd2f024ea9
JMoneyKilla <112935968+JMoneyKilla@users.noreply.github.com>
2023-03-01 16:57:19 +0100

    added some images for the game, can be changed, and edited the layout so it all fits nicely

--- src/rps/gui/controller/GameViewController.java
+++ src/rps/gui/controller/GameViewController.java
@@ -5,7 +5,10 @@ import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,11 +19,13 @@ import java.util.ResourceBundle;
  */
 public class GameViewController implements Initializable {
     @FXML
-    private Label lablePlayerName, lableAIName, lableScore;
+    private AnchorPane anchorPane;
     @FXML
-    private ImageView pictureActiveRPCPlayer, pictureActiveRPCAI, pictureAI;
+    private Label labelHumanName, labelAIName, labelScore, labelAIChat;
     @FXML
-    private MFXTextField textAI;
+    private ImageView imageActiveRPCPlayer, imageActiveRPCAI, imageAIPlayer, imageHumanPlayer, imageVersus, imageChatBox;
+    @FXML
+    private MFXTextField textFieldAI;
     @FXML
     private ImageView imageRockButton, imagePaperButton, imageScissorButton;
 
@@ -30,6 +35,24 @@ public class GameViewController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        Image chatBox = new Image("Pictures/speech_bubble.png");
+        Image versus = new Image("Pictures/versus.png");
+        Image rock = new Image("Pictures/rock.png");
+        Image paper = new Image("Pictures/paper.png");
+        Image scissors = new Image("Pictures/scissors.png");
+        imageChatBox.setImage(chatBox);
+        imageVersus.setImage(versus);
+        imageRockButton.setImage(rock);
+        imagePaperButton.setImage(paper);
+        imageScissorButton.setImage(scissors);
+    }
+
+    public void clickRock(MouseEvent mouseEvent) {
+    }
+
+    public void clickPaper(MouseEvent mouseEvent) {
+    }
+
+    public void clickScissor(MouseEvent mouseEvent) {
     }
 }
