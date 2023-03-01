8c4e1629071c6c9bfca6995be2444762d07b0357
Mads <madsp@hotmail.dk>
2023-03-01 21:02:54 +0100

    added some banter for bots

--- src/rps/gui/JavaFXApp.java
+++ src/rps/gui/JavaFXApp.java
@@ -44,12 +44,58 @@ public class JavaFXApp extends Application {
     GameManager ge = new GameManager(human, bot);
     private String getRandomBotName() {
         String[] botNames = new String[] {
-                "Wallee",
-                "Darlek",
+                "WALL-E",
+                "Dalek",
                 "YoRHa No.2 Type B",
                 "HALL 9000"
         };
         int randomNumber = new Random().nextInt(botNames.length - 1);
         return botNames[randomNumber];
     }
+    private String displayStartOfGameMessage(String botName) {
+        if(botName=="WALL-E"){
+           return "Pleased to meet you";
+        }
+        if(botName=="Dalek"){
+            return "You will be exterminated!";
+        }
+        if(botName=="YoRHa No.2 Type B"){
+            return "Glory to Mankind";
+        }
+        if(botName=="HALL 9000"){
+            return "I am the H.A.L 9000. You may call me Hal";
+        }
+        return "I don't know what to say...";
+    }
+    private String displayGoodMoveMessage(String botName) {
+        if(botName=="WALL-E"){
+            return "Well played!";
+        }
+        if(botName=="Dalek"){
+            return "Exterminate!";
+        }
+        if(botName=="YoRHa No.2 Type B"){
+            return "I underestimated you";
+        }
+        if(botName=="HALL 9000"){
+            return "The 9000 series is the most reliable computer ever made.";
+        }
+        return "I don't know what to say...";
+    }
+    private String displayBadMoveMessage(String botName) {
+        if(botName=="WALL-E"){
+            return "Next time you will win";
+        }
+        if(botName=="Dalek"){
+            return "How could you think that was a good move?";
+        }
+        if(botName=="YoRHa No.2 Type B"){
+            return "Mission sucessful";
+        }
+        if(botName=="HALL 9000"){
+            return "Why are you doing this to me?";
+        }
+        return "I don't know what to say...";
+    }
+
 }
