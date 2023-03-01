2a7db5aa08607d5a2817e5ff45756f205f99deee
Madsp15 <madsp@hotmail.dk>
2023-03-01 11:32:26 +0100

    added some ui stuff

--- src/rps/gui/JavaFXApp.java
+++ src/rps/gui/JavaFXApp.java
@@ -6,6 +6,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import rps.bll.game.GameManager;
+import rps.bll.player.IPlayer;
+import rps.bll.player.Player;
+import rps.bll.player.PlayerType;
+import rps.bll.game.*;
+import rps.bll.player.*;
+import java.util.Random;
 
 /**
  * JavaFX implementation of the RPS game
@@ -25,5 +32,24 @@ public class JavaFXApp extends Application {
         stage.setTitle("Welcome to the not-implemented Rock-Paper-Scissor game!");
         stage.setScene(new Scene(root));
         stage.show();
+
+    }
+
+
+    IPlayer bot = new Player(getRandomBotName(), PlayerType.AI);
+    String playerName = "Player1";
+    IPlayer human = new Player(playerName, PlayerType.Human);
+
+
+    GameManager ge = new GameManager(human, bot);
+    private String getRandomBotName() {
+        String[] botNames = new String[] {
+                "Wallee",
+                "Darlek",
+                "YoRHa No.2 Type B",
+                "HALL 9000"
+        };
+        int randomNumber = new Random().nextInt(botNames.length - 1);
+        return botNames[randomNumber];
     }
 }
